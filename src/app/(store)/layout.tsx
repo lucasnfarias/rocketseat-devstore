b3788de9a7809cdc8909da02301a9d3a6a75f1ad
Lucas Farias <lucas.farias@rtbhouse.com>
2024-08-21 20:20:40 -0300

    create store home, search and loading intial structure

---
+++ src/app/(store)/layout.tsx
@@ -0,0 +1,14 @@
+import { Header } from '@/components/header'
+
+export default function StoreLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <div>
+      <Header />
+      {children}
+    </div>
+  )
+}
